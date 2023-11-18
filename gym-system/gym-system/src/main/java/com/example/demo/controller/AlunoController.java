58a52ef1bde3381efeced1750d6bbdbce8379aa3
maressavlc <148398766+maressavlc@users.noreply.github.com>
2023-11-17 22:25:01 -0300

    Add files via upload
    Utilizamos blocos try-catch para capturar exceções que podem surgir durante a execução dos métodos do controlador. Se uma exceção for lançada, a resposta HTTP será definida para um erro interno do servidor. Atualizações Na aba AlunoController.

--- gym-system/gym-system/src/main/java/com/example/demo/controller/AlunoController.java
+++ gym-system/gym-system/src/main/java/com/example/demo/controller/AlunoController.java
@@ -1,9 +1,6 @@
+
+
 package com.example.demo.controller;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.Aluno;
 import com.example.demo.model.CriarCronogramaRequest;
@@ -12,6 +9,10 @@ import com.example.demo.model.Exercicio;
 import com.example.demo.repository.AlunoRepository;
 import com.example.demo.repository.CronogramaRepository;
 import com.example.demo.repository.ExercicioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +23,7 @@ public class AlunoController {
 
     @Autowired
     private AlunoRepository alunoRepository;
+
     @Autowired
     private ExercicioRepository exercicioRepository;
 
@@ -34,47 +36,70 @@ public class AlunoController {
     }
 
     @GetMapping("/{id}")
-    public Aluno obterAluno(@PathVariable Long id) {
-        return alunoRepository.findById(id).orElse(null);
+    public ResponseEntity<Aluno> obterAluno(@PathVariable Long id) {
+        Optional<Aluno> optionalAluno = alunoRepository.findById(id);
+        return optionalAluno.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Aluno criarAluno(@RequestBody Aluno aluno) {
-        return alunoRepository.save(aluno);
+    public ResponseEntity<Aluno> criarAluno(@RequestBody Aluno aluno) {
+        try {
+            Aluno novoAluno = alunoRepository.save(aluno);
+            return ResponseEntity.status(HttpStatus.CREATED).body(novoAluno);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PutMapping("/{id}")
-    public Aluno atualizarAluno(@PathVariable Long id, @RequestBody Aluno novoAluno) {
+    public ResponseEntity<Aluno> atualizarAluno(@PathVariable Long id, @RequestBody Aluno novoAluno) {
         novoAluno.setId(id);
-        return alunoRepository.save(novoAluno);
+        try {
+            Aluno alunoAtualizado = alunoRepository.save(novoAluno);
+            return ResponseEntity.ok(alunoAtualizado);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deletarAluno(@PathVariable Long id) {
-        alunoRepository.deleteById(id);
-    }
-    @PostMapping("/{alunoId}/criar-cronograma")
-    public ResponseEntity<String> criarCronograma(@PathVariable Long alunoId, @RequestBody CriarCronogramaRequest request) {
-    	Optional<Aluno> optionalAluno = alunoRepository.findById(alunoId);
-
-        if (optionalAluno.isPresent()) {
-            Aluno aluno = optionalAluno.get();
-            List<Exercicio> exercicios = exercicioRepository.findAllById(request.getExercicioIds());
-
-            Cronograma cronograma = new Cronograma();
-            cronograma.setAluno(aluno);
-            cronograma.setExercicios(exercicios);
-            cronograma.setDiaSemana(request.getDiaSemana());
-
-            cronogramaRepository.save(cronograma);
-
-            return ResponseEntity.ok("Cronograma criado com sucesso!");
-        } else {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<Void> deletarAluno(@PathVariable Long id) {
+        try {
+            alunoRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @PostMapping("/{alunoId}/criar-cronograma")
+    public ResponseEntity<String> criarCronograma(@PathVariable Long alunoId, @RequestBody CriarCronogramaRequest request) {
+        try {
+            Optional<Aluno> optionalAluno = alunoRepository.findById(alunoId);
+            if (optionalAluno.isPresent()) {
+                Aluno aluno = optionalAluno.get();
+                List<Exercicio> exercicios = exercicioRepository.findAllById(request.getExercicioIds());
+
+                Cronograma cronograma = new Cronograma();
+                cronograma.setAluno(aluno);
+                cronograma.setExercicios(exercicios);
+                cronograma.setDiaSemana(request.getDiaSemana());
+
+                cronogramaRepository.save(cronograma);
+
+                return ResponseEntity.ok("Cronograma criado com sucesso!");
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     @GetMapping("/exercicios-disponiveis")
     public List<Exercicio> listarExerciciosDisponiveis() {
         return exercicioRepository.findAll();
     }
 }
+
